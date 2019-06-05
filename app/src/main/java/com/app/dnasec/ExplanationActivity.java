9ace1a01a13d8317107904b04b560eb963b51d62
Semyon Pyankov <semyon.pyankov@gmail.com>
2019-06-05 17:55:55 +0300

    Fixed calculations ____________________________________________
    Исправлена ошибка вычислений.

--- app/src/main/java/com/app/dnasec/ExplanationActivity.java
+++ app/src/main/java/com/app/dnasec/ExplanationActivity.java
@@ -102,80 +102,12 @@ public class ExplanationActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                break;
+        if (item.getItemId() == android.R.id.home) {
+            finish();
         }
         return true;
     }
 
-    private int getNucleotideType(String nucleotide) {
-        if (nucleotide.equals(getResources().getString(R.string.adenine))) {
-            return ADENINE;
-        }
-        if (nucleotide.equals(getResources().getString(R.string.uracil))) {
-            return URACIL;
-        }
-        if (nucleotide.equals(getResources().getString(R.string.guanine)) ) {
-            return GUANINE;
-        }
-        if (nucleotide.equals(getResources().getString(R.string.cytosine)) ) {
-            return CYTOSINE;
-        }
-        if (nucleotide.equals(getResources().getString(R.string.thymine))) {
-            return THYMINE;
-        }
-
-        return -1;
-    }
-
-    private String[] parseDNAforTRNA() {
-
-        String[] firstResultNucleotidesPre = firstResult.split("");
-        String[] firstResultNucleotides = new String[firstResultNucleotidesPre.length - 1];
-        if (firstResultNucleotidesPre.length - 1 >= 0)
-            System.arraycopy(firstResultNucleotidesPre, 1, firstResultNucleotides, 0, firstResultNucleotidesPre.length - 1);
-
-        return firstResultNucleotides;
-    }
-
-    private String[] parseIRNAforTRNA() {
-
-        String[] iRNAsequenсePre = secondResult.split("");
-        String[] iRNAsequence = new String[iRNAsequenсePre.length - 1];
-
-        if (iRNAsequenсePre.length - 1 >= 0) {
-            System.arraycopy(iRNAsequenсePre, 1, iRNAsequence, 0, iRNAsequenсePre.length - 1);
-        }
-
-        return iRNAsequence;
-
-    }
-
-    private String[] parseTRNA() {
-        String[] secondResultPrePre = secondResult.split("; ");
-        StringBuilder formattedSecondResult = new StringBuilder();
-        for (String s : secondResultPrePre) {
-            formattedSecondResult.append(s);
-        }
-        String[] secondResultPre = formattedSecondResult.toString().split("");
-        String[] secondResult = new String[secondResultPre.length - 1];
-        if (secondResultPre.length - 1 >= 0)
-            System.arraycopy(secondResultPre, 1, secondResult, 0, secondResultPre.length - 1);
-
-        return secondResult;
-    }
-
-    private String[] parseAminoacid() {
-        String[] aminoacidPre = thirdResult.split("-");
-        String[] aminoacid = new String[aminoacidPre.length - 1];
-
-        if (aminoacidPre.length - 1 >= 0)
-            System.arraycopy(aminoacidPre, 1, aminoacid, 0, aminoacidPre.length - 1);
-
-        return aminoacid;
-    }
 
     class HandleRecyclers extends AsyncTask<Void, Void, ArrayList> {
 
@@ -200,30 +132,47 @@ public class ExplanationActivity extends AppCompatActivity {
             switch (sequenceType) {
                 case DNA_SEQUENCE:
                     sequenceName.setText(getResources().getString(R.string.DNA));
+//                    String[] secondResult = new String[] {};
                     if (!DnaIsMatrix) {
                         linkedText.setText(R.string.DNA_transcription_explanation);
                         secondExpl.setText(R.string.DNA_translation_explanation);
                         thirdExpl.setText(R.string.DNA_acid_synthesis_explanation);
+//                        secondResult = parseTRNA();
                     } else {
                         linkedText.setText(R.string.DNA_is_matrix_first_step);
                         secondExpl.setText(R.string.DNA_is_matrix_second_step);
                         thirdExpl.setText(R.string.DNA_is_matrix_third_step);
                         ((TextView) findViewById(R.id.first_result_bottom)).setText(R.string.tRNA);
-                        ((TextView) findViewById(R.id.second_result_bottom)).setText(R.string.iRNA);
-                        ((TextView) findViewById(R.id.second_result_top)).setText(R.string.tRNA);
-                    }
-
-                    for (int i = 0; i < sequenceNucleotides.length; i++) {
-                        pairs1.add(
-                                new PairModel(
-                                        getNucleotideType(sequenceNucleotides[i]),
-                                        getNucleotideType(firstResultNucleotides[i]),
-                                        new NucleotidesPair(getApplicationContext())
-                                )
-                        );
+                        ((TextView) findViewById(R.id.second_result_bottom)).setText(R.string.tRNA);
+                        ((TextView) findViewById(R.id.second_result_top)).setText(R.string.iRNA);
+                        ((ImageView)findViewById(R.id.arrow_2)).setImageResource(R.drawable.ic_up_arrow);
                     }
 
                     String[] secondResult = parseTRNA();
+
+                    if (DnaIsMatrix) {
+                        for (int i = 0; i < sequenceNucleotides.length; i++) {
+                            pairs1.add(
+                                    new PairModel(
+                                            getNucleotideType(sequenceNucleotides[i]),
+                                            getNucleotideType(secondResult[i]),
+                                            new NucleotidesPair(getApplicationContext())
+                                    )
+                            );
+                        }
+                    } else {
+                        for (int i = 0; i < sequenceNucleotides.length; i++) {
+                            pairs1.add(
+                                    new PairModel(
+                                            getNucleotideType(sequenceNucleotides[i]),
+                                            getNucleotideType(firstResultNucleotides[i]),
+                                            new NucleotidesPair(getApplicationContext())
+                                    )
+                            );
+                        }
+                    }
+
+
                     for (int i = 0; i < secondResult.length; i++) {
                         pairs2.add(
                                 new PairModel(
@@ -367,7 +316,7 @@ public class ExplanationActivity extends AppCompatActivity {
 
             firstResultList = findViewById(R.id.first_tesult_list);
             firstResultList.setAdapter((RecyclerView.Adapter)explanationAdapter.get(0));
-            ((ProgressBar)findViewById(R.id.progressBar_1)).setVisibility(View.GONE);
+            findViewById(R.id.progressBar_1).setVisibility(View.GONE);
             firstResultList.setLayoutManager(horizontalLayout);
 
 
@@ -375,7 +324,7 @@ public class ExplanationActivity extends AppCompatActivity {
                     = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
             RecyclerView secondResultList = findViewById(R.id.second_result_list);
             secondResultList.setAdapter((RecyclerView.Adapter)explanationAdapter.get(1));
-            ((ProgressBar)findViewById(R.id.progressBar_2)).setVisibility(View.GONE);
+            findViewById(R.id.progressBar_2).setVisibility(View.GONE);
             secondResultList.setLayoutManager(horizontalLayout1);
 
 
@@ -383,10 +332,77 @@ public class ExplanationActivity extends AppCompatActivity {
                     = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
             RecyclerView thirdResultList = findViewById(R.id.third_result_list);
             thirdResultList.setAdapter((RecyclerView.Adapter)explanationAdapter.get(2));
-            ((ProgressBar)findViewById(R.id.progressBar_3)).setVisibility(View.GONE);
+            findViewById(R.id.progressBar_3).setVisibility(View.GONE);
             thirdResultList.setLayoutManager(horizontalLayout2);
 
         }
+    }
+
+    private int getNucleotideType(String nucleotide) {
+        if (nucleotide.equals(getResources().getString(R.string.adenine))) {
+            return ADENINE;
+        }
+        if (nucleotide.equals(getResources().getString(R.string.uracil))) {
+            return URACIL;
+        }
+        if (nucleotide.equals(getResources().getString(R.string.guanine)) ) {
+            return GUANINE;
+        }
+        if (nucleotide.equals(getResources().getString(R.string.cytosine)) ) {
+            return CYTOSINE;
+        }
+        if (nucleotide.equals(getResources().getString(R.string.thymine))) {
+            return THYMINE;
+        }
+
+        return -1;
+    }
+
+    private String[] parseDNAforTRNA() {
+
+        String[] firstResultNucleotidesPre = firstResult.split("");
+        String[] firstResultNucleotides = new String[firstResultNucleotidesPre.length - 1];
+        if (firstResultNucleotidesPre.length - 1 >= 0)
+            System.arraycopy(firstResultNucleotidesPre, 1, firstResultNucleotides, 0, firstResultNucleotidesPre.length - 1);
+
+        return firstResultNucleotides;
+    }
+
+    private String[] parseIRNAforTRNA() {
+
+        String[] iRNAsequencePre = secondResult.split("");
+        String[] iRNAsequence = new String[iRNAsequencePre.length - 1];
+
+        if (iRNAsequencePre.length - 1 >= 0) {
+            System.arraycopy(iRNAsequencePre, 1, iRNAsequence, 0, iRNAsequencePre.length - 1);
+        }
+
+        return iRNAsequence;
+
+    }
+
+    private String[] parseTRNA() {
+        String[] secondResultPrePre = secondResult.split("; ");
+        StringBuilder formattedSecondResult = new StringBuilder();
+        for (String s : secondResultPrePre) {
+            formattedSecondResult.append(s);
+        }
+        String[] secondResultPre = formattedSecondResult.toString().split("");
+        String[] secondResult = new String[secondResultPre.length - 1];
+        if (secondResultPre.length - 1 >= 0)
+            System.arraycopy(secondResultPre, 1, secondResult, 0, secondResultPre.length - 1);
+
+        return secondResult;
+    }
+
+    private String[] parseAminoacid() {
+        String[] aminoacidPre = thirdResult.split("-");
+        String[] aminoacid = new String[aminoacidPre.length - 1];
+
+        if (aminoacidPre.length - 1 >= 0)
+            System.arraycopy(aminoacidPre, 1, aminoacid, 0, aminoacidPre.length - 1);
+
+        return aminoacid;
     }
 
 }
